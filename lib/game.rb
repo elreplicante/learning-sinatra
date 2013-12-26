5aeb9eb5b7f9daa197d22225087d80a7ff5ebca4
Sergio Revilla <sergio@unmundotecnologico.org>
2013-12-26 13:17:16 +0100

    Headers

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
+require 'uuid'
 
 before do
   content_type :txt
   @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
+  @guid = UUID.new.generate
 end
 
 get '/throw/:type' do
   player_throw = params[:type].to_sym
+
   if !@throws.include?(player_throw)
     halt 403, "You must throw one of the following  #{@throws}"
   end
@@ -21,4 +24,23 @@ get '/throw/:type' do
   else
     "You lose!"
   end
+end
+
+get '/request' do
+  request.env.map { |e| e.to_s + '\n'  }
+end
+
+
+get '/request_methods' do
+  request.methods.map { |e| e.to_s + '\n'  }
+end
+
+get '/caching' do
+  expires 3600, :public, :must_revalidate
+    "This page rendered at #{Time.now}"
+end
+
+get '/etag' do
+  etag @guid
+  "This resource has an ETag value of #{@guid}"
 end
